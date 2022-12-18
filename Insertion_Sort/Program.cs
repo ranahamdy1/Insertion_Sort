5271ca005dc53cea6766aed00606a4614e2f8594
Your Name <you@example.com>
2022-12-18 23:15:28 +0200

    initial commit

---
+++ Insertion_Sort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 9, 2, 0, 4, 8 };
+            InsertionSort(a);
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+            }
+            Console.ReadLine();
+        }
+        private static void InsertionSort(int[] a)
+        {
+            int cnt = a.Length;
+            int key;
+            for (int i = 0; i < cnt; i++)
+            {
+                key = a[i];
+                int j = i - 1;
+                while (j >= 0 && a[j] > key)
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+                a[j + 1] = key;
+            }
+        }
+    }
+}
